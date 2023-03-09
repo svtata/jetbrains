f0c642a60bc64c2471f28a800037ffac6a15efa1
Tatiana Sverchkova <swerchkova.tatiana@gmail.com>
2023-03-10 02:15:27 +0400

    Added env variables

--- src/test/java/com/jetbrains/TestData.java
+++ src/test/java/com/jetbrains/TestData.java
@@ -3,8 +3,9 @@ package com.jetbrains;
 import java.util.List;
 
 public class TestData {
-    public final static String USERNAME = System.getProperty("username");
-    public final static String PASSWORD = System.getProperty("password");
+
+    public final static String USERNAME = System.getenv("USERNAME");
+    public final static String PASSWORD = System.getenv("PASSWORD");
     public final static List<String> TABS = List.of("Developer Tools", "Team Tools",
             "Education", "Solutions", "Support",
             "Store");
